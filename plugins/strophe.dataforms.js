93ca50044f6cedd79f01c60aceb051833379a9f1
François de Metz <francois@2metz.fr>
2010-03-12 23:44:13 +0100

    initial version of dataforms plugins, only support parse method

---
+++ plugins/strophe.dataforms.js
@@ -0,0 +1,141 @@
+/*
+  Copyright 2010, François de Metz <francois@2metz.fr>
+*/
+/**
+ *
+ */
+Strophe.Field = function(field) {
+    /**
+     * @type String
+     */
+    this.type = field.getAttribute("type");
+    /**
+     * @type String
+     */
+    this.variable = field.getAttribute("var");
+    /**
+     * @type String
+     */
+    this.label = field.getAttribute("label");
+    /**
+     * @type Boolean
+     */
+    this.required = this._isRequired(field);
+    /**
+     * @type String
+     */
+    this.desc = this._getDesc(field);
+    /**
+     * @type String
+     */
+    this.value = null;
+    /**
+     * @type Array
+     */
+    this.values = [];
+    this._fillValues(field);
+    /**
+     * @type Array
+     */
+    this.options = this._parseOptions(field);
+    if (this.type != "fixed" && this.variable == null) {
+        throw "must have a var attribute";
+    }
+};
+Strophe.Field.prototype = {
+    _isRequired: function(field) {
+        var required = field.getElementsByTagName("required");
+        if (required.length == 1) {
+            return true;
+        }
+        return false;
+    },
+
+    _getDesc: function(field) {
+        var desc = field.getElementsByTagName("desc");
+        if (desc.length == 1) {
+            return desc.item(0).textContent;
+        }
+        return null;
+    },
+
+    _fillValues: function(field) {
+        var values = field.getElementsByTagName("value");
+        if (values.length > 1) {
+            var authorized = ["list-multi", "jid-multi",
+                              "text-multi", "hidden"];
+            if (authorized.indexOf(this.type) == -1) {
+                throw "bad";
+            }
+            for (var i = 0; i < values.length; i++) {
+                this.values.push(values.item(i).textContent);
+            }
+        } else if (values.length == 1) {
+            this.value = values.item(0).textContent;
+        }
+    },
+
+    _parseOptions: function(field) {
+        var options = field.getElementsByTagName("option");
+        if (options.length == 0) {
+            return [];
+        }
+        var authorized = ["list-single", "list-multi"];
+        if (authorized.indexOf(this.type) == -1) {
+            throw "bad";
+        }
+        var o = [];
+        for (var i = 0; i < options.length; i++) {
+            var value = this._getValue(options.item(i));
+            var label = options.item(i).getAttribute("label") || value;
+            o.push({value: value, label: label});
+        }
+        return o;
+    },
+
+    _getValue : function(node) {
+        var value = node.getElementsByTagName("value");
+        if (value.length > 1 || value.length == 0) {
+            throw "bad";
+        }
+        return value.item(0).textContent;
+    }
+};
+
+/**
+ * Data Forms strophe plugin
+ * http://xmpp.org/extensions/xep-0004.html
+ */
+Strophe.addConnectionPlugin('dataforms',
+{
+    init : function() {},
+
+    parse: function(form)
+    {
+        return {
+            type : form.getAttribute("type"),
+            // TODO: multiple title
+            title : this._getTitle(form),
+            // TODO: multiple instructions
+            intructions : [],
+            fields : this._parseFields(form)
+        };
+    },
+
+    _getTitle: function(form) {
+        var title = form.getElementsByTagName("title");
+        if (title.length > 0) {
+            return title.item(0).textContent;
+        }
+        return null;
+    },
+
+    _parseFields: function(form) {
+        var fields = form.getElementsByTagName("field");
+        var f = [];
+        for (var i = 0; i < fields.length; i++) {
+            f.push(new Strophe.Field(fields.item(i)));
+        }
+        return f;
+    }
+});
---
+++ tests/dataforms.js
@@ -0,0 +1,234 @@
+module('plugins.DataForms');
+function getForm(type, title) {
+    if (!type) {
+        type = "form";
+    }
+    if (!title) {
+        title = "Test dataForm";
+    }
+    try {
+        var form = document.createElementNS("jabber:x:data", "x");
+    } catch (e) {
+        var form = document.createElement("x");
+        form.setAttribute("xmlns", "jabber:x:data");
+    }
+    form.setAttribute("type", type);
+    var titleNode = document.createElement("title");
+    titleNode.textContent = title;
+    form.appendChild(titleNode);
+    return form;
+}
+
+var dataformsPlugin = Strophe._connectionPlugins.dataforms;
+
+test("parse type and title", function() {
+         var form = dataformsPlugin.parse(getForm());
+         equal("form", form.type);
+         equal("Test dataForm", form.title);
+     });
+
+
+test("parse another form", function() {
+         var form = dataformsPlugin.parse(getForm("result", "Test Form"));
+         equal("result", form.type);
+         equal("Test Form", form.title);
+     });
+
+test("parse fields", function() {
+         var form = getForm("result");
+         var field = document.createElement("field");
+         field.setAttribute("type", "text-single");
+         field.setAttribute("var", "field");
+         form.appendChild(field);
+         var dataForm = dataformsPlugin.parse(form);
+         equal(1, dataForm.fields.length);
+         equal("text-single", dataForm.fields[0].type);
+     });
+
+test("parse field", function() {
+         var field = document.createElement("field");
+         field.setAttribute("var", "name-of-field");
+         field.setAttribute("type", "text-single");
+         var form = new Strophe.Field(field);
+         equal("name-of-field", form.variable);
+         equal("text-single", form.type);
+     });
+
+test("parse field without var attribute throw exception", function() {
+         expect(1);
+        var field = document.createElement("field");
+        field.setAttribute("type", "text-single");
+        try {
+             var form = new Strophe.Field(field);
+        } catch (e) {
+            ok(true);
+        }
+     });
+
+test("parse field type fixed without var attribute", function() {
+         expect(1);
+         var field = document.createElement("field");
+         field.setAttribute("type", "fixed");
+         try {
+             var form = new Strophe.Field(field);
+             ok(true);
+         } catch (e) {
+
+         }
+     });
+
+test("parse label field", function() {
+         var field = document.createElement("field");
+         field.setAttribute("type", "text-single");
+         field.setAttribute("var", "test");
+         field.setAttribute("label", "hello");
+         var form = new Strophe.Field(field);
+         equal("hello", form.label);
+     });
+
+test("parse required field", function() {
+         var field = document.createElement("field");
+         field.setAttribute("type", "text-single");
+         field.setAttribute("var", "test");
+         field.appendChild(document.createElement("required"));
+         var form = new Strophe.Field(field);
+         equal(true, form.required);
+     });
+
+test("parse non required field", function() {
+         var field = document.createElement("field");
+         field.setAttribute("type", "text-single");
+         field.setAttribute("var", "test");
+         var form = new Strophe.Field(field);
+         equal(false, form.required);
+     });
+
+test("parse field description", function() {
+         var field = document.createElement("field");
+         field.setAttribute("type", "text-single");
+         field.setAttribute("var", "test");
+         var desc = document.createElement("desc");
+         desc.textContent = "descriptionhasatooltip";
+         field.appendChild(desc);
+         var form = new Strophe.Field(field);
+         equal("descriptionhasatooltip", form.desc);
+     });
+
+test("parse field value", function() {
+         var field = document.createElement("field");
+         field.setAttribute("type", "text-single");
+         field.setAttribute("var", "test");
+         var value = document.createElement("value");
+         value.textContent = "onevalue";
+         field.appendChild(value);
+         var form = new Strophe.Field(field);
+         equal("onevalue", form.value);
+     });
+
+test("parse multiple value", function() {
+         var field = document.createElement("field");
+         field.setAttribute("type", "text-multi");
+         field.setAttribute("var", "test");
+         var value = document.createElement("value");
+         value.textContent = "onevalue";
+         field.appendChild(value);
+         var value2 = document.createElement("value");
+         value2.textContent = "onevaluetoo";
+         field.appendChild(value2);
+         var form = new Strophe.Field(field);
+         equal(2, form.values.length);
+         equal("onevalue", form.values[0]);
+         equal("onevaluetoo", form.values[1]);
+     });
+
+test("on parse multiple value in non multiple value field throw exception", function() {
+         expect(1);
+         var field = document.createElement("field");
+         field.setAttribute("type", "text-single");
+         field.setAttribute("var", "test");
+         var value = document.createElement("value");
+         value.textContent = "onevalue";
+         field.appendChild(value);
+         var value2 = document.createElement("value");
+         value2.textContent = "onevaluetoo";
+         field.appendChild(value2);
+         try {
+             var form = Strophe.Field(field);
+         } catch (e) {
+             ok(true);
+         }
+     });
+
+test("on parse option in non list field throw exception", function() {
+         expect(1);
+         var field = document.createElement("field");
+         field.setAttribute("type", "text-single");
+         field.setAttribute("var", "test");
+         var option = document.createElement("option");
+         var value = document.createElement("value");
+         value.textContent = "oneoption";
+         option.appendChild(value);
+         field.appendChild(option);
+         try {
+             var form = new Strophe.Field(field);
+         } catch (e) {
+             ok(true);
+         }
+     });
+
+test("parse list-single", function() {
+         var field = document.createElement("field");
+         field.setAttribute("type", "list-single");
+         field.setAttribute("var", "test");
+         var option = document.createElement("option");
+         var value = document.createElement("value");
+         value.textContent = "oneoption";
+         option.appendChild(value);
+         field.appendChild(option);
+         var form = new Strophe.Field(field);
+         equal(1, form.options.length);
+         equal("oneoption", form.options[0].value);
+         equal("oneoption", form.options[0].label);
+     });
+
+test("parse list-multi field", function() {
+         var field = document.createElement("field");
+         field.setAttribute("type", "list-multi");
+         field.setAttribute("var", "test");
+         var option = document.createElement("option");
+         option.setAttribute("label", "onelabel");
+         var value = document.createElement("value");
+         value.textContent = "oneoption";
+         option.appendChild(value);
+         field.appendChild(option);
+         var option2 = document.createElement("option");
+         var value2 = document.createElement("value");
+         value2.textContent = "oneoptiontoo";
+         option2.appendChild(value2);
+         field.appendChild(option2);
+         var form = new Strophe.Field(field);
+         equal(2, form.options.length);
+         equal("oneoption", form.options[0].value);
+         equal("onelabel", form.options[0].label);
+     });
+
+test("parse 2 value in one option throw exception", function() {
+         expect(1);
+         var field = document.createElement("field");
+         field.setAttribute("type", "list-multi");
+         field.setAttribute("var", "test");
+         var option = document.createElement("option");
+         option.setAttribute("label", "onelabel");
+         var value = document.createElement("value");
+         value.textContent = "oneoption";
+         option.appendChild(value);
+         var value2 = document.createElement("value");
+         value2.textContent = "onevalue";
+         option.appendChild(value2);
+         field.appendChild(option);
+         try {
+             var form = new Strophe.Field(field);
+         } catch (e) {
+             ok(true);
+         }
+     });
